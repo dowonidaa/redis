9c15dadabd6781c3bb7de37f01af44f7799d86c0
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-07 15:43:52 +0900

    init commit

---
+++ practice-4-my/src/main/java/com/example/redis/practice_4_my/Practice4MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis.practice_4_my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice4MyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice4MyApplication.class, args);
+	}
+
+}
---
+++ practice-4-my/src/test/java/com/example/redis/practice_4_my/Practice4MyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis.practice_4_my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice4MyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
